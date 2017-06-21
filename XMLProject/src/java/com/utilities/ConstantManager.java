aeda12e3618db5a9ed0647d0593449a1f5097fff
LONGDDSE62137\Du <long020396@gmail.com>
2017-06-21 17:51:07 +0700

    this is Long test commit

---
+++ XMLProject/src/java/com/utilities/ConstantManager.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utilities;
+
+/**
+ *
+ * @author Du
+ */
+public class ConstantManager {
+    public static final String homepage = "www.bulovo.com";
+}
